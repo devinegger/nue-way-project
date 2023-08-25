dabe48478dc226835a72e6aea3dad511f347f3c4
devinegger <itsme@devinegger.cc>
2023-08-24 23:41:08 -0400

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import EcosystemIcon from '../components/icons/IconEcosystem.vue'
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      <i>
+        <EcosystemIcon />
+        Devin is super cool, everyone!
+      </i>
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+  }
+  
+  p {
+    margin-top: 2rem;
+  }
+}
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="contact">
+        This is the Contact page, yo!
+    </div>
+</template>
+
+<style>
+    .contact {
+        display: flex;
+        place-content: center;
+    }
+</style>
